1df2717e39a1a24918e60ffc589699ab0a37faf8
Quivscor <quivscor@gmail.com>
2020-11-27 19:01:16 +0100

    Locomotive

---
+++ Assets/Pociagi/Scripts/Train/InputManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputData Data { get; private set; }
+    void Awake()
+    {
+        Data = new InputData();
+    }
+
+    void Update()
+    {
+        //movement
+        Data.moveX = Input.GetAxis("Horizontal");
+        Data.moveY = Input.GetAxis("Vertical");
+
+        //actions
+        Data.isBrake = Input.GetKey(KeyCode.Space);
+    }
+}
+
+public class InputData
+{
+    public float moveX;
+    public float moveY;
+    public bool isBrake;
+}
---
+++ Assets/Pociagi/Scripts/Train/TrainController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainController : MonoBehaviour
+{
+    public float accelerationForce;
+    public float torqueForce;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 velocity = InputManager.Data.moveY * transform.forward;
+        Vector3 torque = InputManager.Data.moveX * transform.up;
+
+        rb.AddForce(velocity * accelerationForce);
+        rb.AddTorque(torque * torqueForce);
+    }
+}
