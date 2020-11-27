128b3310c7d6bbbba9b209d6f82b7fe683d3b57f
Quivscor <quivscor@gmail.com>
2020-11-27 23:32:58 +0100

    Acceleration rate added

--- Assets/Trains/Scripts/Train/TrainController.cs
+++ Assets/Trains/Scripts/Train/TrainController.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class TrainController : MonoBehaviour
 {
-    public float accelerationForce;
+    private TrainMovement trainMovement = TrainMovement.Stopped;
+
+    public float maxVelocity;
     public float torqueForce;
 
     public float maxAngularVelocity = 1.5f;
-    public float maxVelocity = 5f;
 
-    private Vector3 forwardSmoothing;
-    private Vector3 angularSmoothing;
-    public float forwardSmoothingTime;
     public float angularSmoothingTime;
-    public float brakingSmoothingTime;
+    public float minVelocityToRotate;
+
+    public float maxAccelerationTime = 3f;
+    private float currentAccelerationTime;
 
     private bool isOnIce = false;
 
@@ -29,19 +30,55 @@ public class TrainController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 velocity = InputManager.Data.moveY > 0 ? InputManager.Data.moveY * transform.forward : Vector3.zero;
+        ForwardMovement();
+        //Vector3 velocity = InputManager.Data.moveY > 0 ? InputManager.Data.moveY * transform.forward : Vector3.zero;
 
-        if(InputManager.Data.isBrake)
-            rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, brakingSmoothingTime);
-        else if(velocity != Vector3.zero)
-            rb.velocity = Vector3.Lerp(rb.velocity, velocity * accelerationForce, forwardSmoothingTime);
+        //if (InputManager.Data.isBrake)
+        //    rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, brakingSmoothingTime);
+        //else if (velocity != Vector3.zero )
+        //{
+        //    //rb.velocity = Vector3.Lerp(rb.velocity, velocity * maxVelocity, forwardSmoothingTime);
+        //    
+        //    rb.velocity = Vector3.Lerp(Vector3.zero, velocity * maxVelocity, (currentAccelerationTime/maxAccelerationTime));
+        //}
+        //else
+        //    
 
 
-        if (velocity != Vector3.zero)
+
+        //turning
+        if (trainMovement != TrainMovement.Stopped && Vector3.Magnitude(rb.velocity) > minVelocityToRotate)
         {
             Vector3 torqueDirection = new Vector3(0, InputManager.Data.moveX != 0 ? Mathf.Sign(InputManager.Data.moveX) : 0, 0);
             rb.angularVelocity = Vector3.Lerp(rb.angularVelocity, torqueDirection * torqueForce * Time.fixedDeltaTime, angularSmoothingTime);
             //rb.angularVelocity = Vector3.SmoothDamp(transform.rotation.eulerAngles, torqueDirection * torqueForce, ref angularSmoothing, angularSmoothingTime);
         }
     }
+
+    private void ForwardMovement()
+    {
+        if(InputManager.Data.moveY > 0)
+        {
+            trainMovement = TrainMovement.Accelerating;
+            currentAccelerationTime = currentAccelerationTime >= maxAccelerationTime ? maxAccelerationTime : currentAccelerationTime + Time.fixedDeltaTime;
+        }
+        else if(currentAccelerationTime > 0)
+        {
+            trainMovement = TrainMovement.Decelerating;
+            currentAccelerationTime = currentAccelerationTime <= 0 ? 0 : currentAccelerationTime - Time.fixedDeltaTime;
+        }
+        else
+        {
+            trainMovement = TrainMovement.Stopped;
+        }
+
+        rb.velocity = Vector3.Lerp(Vector3.zero, maxVelocity * transform.forward, (currentAccelerationTime / maxAccelerationTime));
+    }
+}
+
+public enum TrainMovement
+{
+    Stopped,
+    Accelerating,
+    Decelerating
 }
