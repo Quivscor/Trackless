ed6009aee2e538cfad9910dff1b6756db9c8ae7a
Quivscor <quivscor@gmail.com>
2020-11-27 21:21:29 +0100

    Changes to train controller

--- Assets/Trains/Scripts/Train/TrainController.cs
+++ Assets/Trains/Scripts/Train/TrainController.cs
@@ -7,20 +7,32 @@ public class TrainController : MonoBehaviour
     public float accelerationForce;
     public float torqueForce;
 
+    public float maxAngularVelocity = 1.5f;
+    public float maxVelocity = 5f;
+
+    private bool isOnIce = false;
+
     private Rigidbody rb;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+
+        rb.maxAngularVelocity = maxAngularVelocity;
     }
 
     private void FixedUpdate()
     {
-        Vector3 velocity = InputManager.Data.moveY * transform.forward;
-        Vector3 torque = InputManager.Data.moveX * transform.up;
+        Vector3 velocity = InputManager.Data.moveY > 0 ? InputManager.Data.moveY * transform.forward : Vector3.zero;
+        Vector3 torque = Vector3.zero;
 
         if(velocity != Vector3.zero)
-            rb.AddForce(velocity * accelerationForce);
-        rb.AddTorque(torque * torqueForce);
+            rb.AddRelativeForce(velocity * accelerationForce);
+
+        if (rb.velocity != Vector3.zero)
+        {
+            torque = InputManager.Data.moveX * transform.up;
+            rb.AddRelativeTorque(torque * torqueForce);
+        }
     }
 }
