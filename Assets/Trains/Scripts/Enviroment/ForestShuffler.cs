2deb8bb3ff8bbb9aa5390d12c0d0aa883009af41
B.Krukowski <38346579+OrangeInside@users.noreply.github.com>
2020-11-28 19:55:05 +0100

    Forest shuffler

---
+++ Assets/Trains/Scripts/Enviroment/ForestShuffler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForestShuffler : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> trees;
+
+    void Start()
+    {
+        foreach (GameObject tree in trees)
+        {
+            if(Random.Range(0, 100) > 80)
+                tree.SetActive(false);
+        }
+        /*
+        int toRemove = Random.Range(0, trees.Count - 1);
+        while(toRemove > 0)
+        {
+            int removedTree = Random.Range(0, trees.Count);
+            trees[removedTree].SetActive(false);
+            trees.RemoveAt(removedTree);
+            toRemove--;
+        }*/
+
+    }
+
+}
