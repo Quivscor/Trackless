132355eb4b486b1ba69596abaa1f14314a8cb205
Quivscor <quivscor@gmail.com>
2020-11-28 21:27:19 +0100

    Snowstorm

---
+++ Assets/SnowstormPropertiesAccessor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class SnowstormPropertiesAccessor : MonoBehaviour
+{
+    private static VisualEffect snowstormEffect;
+
+    private void Awake()
+    {
+        snowstormEffect = GetComponent<VisualEffect>();
+    }
+
+    public static void IncreaseSnowStrength()
+    {
+        snowstormEffect.SetVector2("Range", new Vector2(5000, 10000));
+    }
+
+    public static void DecreaseSnowStrength()
+    {
+        snowstormEffect.SetVector2("Range", new Vector2(500, 1000));
+    }
+}
---
+++ Assets/StrongSnowEnabler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrongSnowEnabler : MonoBehaviour
+{
+    public LayerMask locomotiveMask;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == (other.gameObject.layer | (1 << locomotiveMask)))
+        {
+            SnowstormPropertiesAccessor.IncreaseSnowStrength();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == (other.gameObject.layer | (1 << locomotiveMask)))
+        {
+            SnowstormPropertiesAccessor.DecreaseSnowStrength();
+        }
+    }
+}
