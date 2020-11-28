7feca8aa89c4c26324ce8b77cd4a18f9f42f6dba
Zawisz <zawisza_mateusz@wp.pl>
2020-11-28 19:46:05 +0100

    New trees, steel car improved

--- Assets/Trains/Scripts/RandomRotation.cs
+++ Assets/Trains/Scripts/RandomRotation.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomRotation : MonoBehaviour
-{ 
+{
     void Start()
     {
-        
+
         this.transform.Rotate(new Vector3(0, Random.Range(0, 360f), 0));
-        float value = Random.Range(-0.3f, 0.6f);
+        float value = Random.Range(-0.75f, 0.75f);
         Vector3 scale = new Vector3(value, value, value);
         this.transform.localScale += scale;
     }
