5c8a207fc599f6facd61520eebd9bc6ed2393f84
Zawisz <zawisza_mateusz@wp.pl>
2020-11-28 22:27:04 +0100

    Old Particles update

---
+++ Assets/Trains/Scripts/General/VisualEffectSpeed.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class VisualEffectSpeed : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    public VisualEffect visualEffect;
+
+    private void Awake()
+    {
+        visualEffect = GetComponent<VisualEffect>();
+    }
+
+    void Update()
+    {
+        visualEffect.playRate = speed;
+    }
+}
--- Assets/Trains/Scripts/Train/WagonRotator.cs
+++ Assets/Trains/Scripts/Train/WagonRotator.cs
@@ -7,8 +7,14 @@ public class WagonRotator : MonoBehaviour
     [SerializeField]
     private float maxRotation = 10.0f;
 
+    public GameObject leftSideSparks;
+    public GameObject rightSideSparks;
+
     public void SetPercentageRotation(float percentageValue)
     {
         this.transform.localRotation = Quaternion.Euler(this.transform.localRotation.eulerAngles.x, this.transform.localRotation.eulerAngles.y, percentageValue * maxRotation / 100.0f);
+
+        leftSideSparks?.SetActive(this.transform.localRotation.eulerAngles.z > 8.0f && this.transform.localRotation.eulerAngles.z < 180.0f);
+        rightSideSparks?.SetActive(this.transform.localRotation.eulerAngles.z < 352.0f && this.transform.localRotation.eulerAngles.z > 180.0f);
     }
 }
