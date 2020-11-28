dd2e8feacd095c3d8c2bbdccd2d2322df6c80669
B.Krukowski <38346579+OrangeInside@users.noreply.github.com>
2020-11-28 02:59:39 +0100

    Tree shake animation

---
+++ Assets/TreeAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeAnimation : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.enabled = true;
+        animator.Play("tree_shake");
+    }
+
+    public void TurnOffAnimator()
+    {
+        animator.enabled = false;
+    }
+}
