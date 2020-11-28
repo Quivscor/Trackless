d23b3d2940981bc48eb94afb3d795b4f8c788d4e
Quivscor <quivscor@gmail.com>
2020-11-28 12:50:11 +0100

    Reversing train

--- Assets/Trains/Scripts/Train/TrainController.cs
+++ Assets/Trains/Scripts/Train/TrainController.cs
@@ -12,6 +12,7 @@ public class TrainController : MonoBehaviour
 
     public const float maxAngularVelocityForRotation = 0.6f;
 
+    public float reverseMaxVelocity;
     public float minVelocity;
     public float maxVelocity;
     public float torqueForce;
@@ -24,6 +25,11 @@ public class TrainController : MonoBehaviour
 
     public float maxAccelerationTime = 3f;
     private float currentAccelerationTime;
+
+    private float reverseAccelerationTime;
+    public float maxReverseAccelerationTime = 1f;
+
+    public float wallHitVelocityThreshold;
 
     [Header("Ice")]
     public LayerMask iceMask;
@@ -65,24 +71,101 @@ public class TrainController : MonoBehaviour
     {
         float additionalMovement = isOnIce ? iceDecelerationMultiplier : 1f;
 
-        if (InputManager.Data.moveY > 0)
+        SetTrainState();
+        //if (InputManager.Data.moveY > 0)
+        //{
+        //    trainMovement = TrainMovement.Accelerating;
+        //    currentAccelerationTime = currentAccelerationTime >= maxAccelerationTime ? maxAccelerationTime : currentAccelerationTime + (Time.fixedDeltaTime * iceDecelerationMultiplier);
+        //}
+        //else if (currentAccelerationTime > 0)
+        //{
+        //    trainMovement = TrainMovement.Decelerating;
+        //    currentAccelerationTime = currentAccelerationTime <= 0 ? 0 : currentAccelerationTime - (Time.fixedDeltaTime / iceDecelerationMultiplier);
+        //}
+        //else if (InputManager.Data.moveY == 0)
+        //{
+        //    trainMovement = TrainMovement.Stopped;
+        //}
+
+        if(trainMovement != TrainMovement.Reversing)
         {
-            trainMovement = TrainMovement.Accelerating;
-            currentAccelerationTime = currentAccelerationTime >= maxAccelerationTime ? maxAccelerationTime : currentAccelerationTime + (Time.fixedDeltaTime * iceDecelerationMultiplier);
-        }
-        else if (currentAccelerationTime > 0)
-        {
-            trainMovement = TrainMovement.Decelerating;
-            currentAccelerationTime = currentAccelerationTime <= 0 ? 0 : currentAccelerationTime - (Time.fixedDeltaTime / iceDecelerationMultiplier);
+            rb.velocity = Vector3.Lerp(minVelocity * transform.forward, maxVelocity * transform.forward, (currentAccelerationTime / maxAccelerationTime));
         }
         else
         {
-            trainMovement = TrainMovement.Stopped;
+            rb.velocity = Vector3.Lerp(Vector3.zero, reverseMaxVelocity * transform.forward * -1, (reverseAccelerationTime / maxReverseAccelerationTime));
         }
-        rb.velocity = Vector3.Lerp(minVelocity * transform.forward, maxVelocity * transform.forward, (currentAccelerationTime / maxAccelerationTime));
+
         trainManager.SetWheelsRotationSpeed((currentAccelerationTime < 0 ? 0 : currentAccelerationTime) / maxAccelerationTime * 100);
         snowRide.SetPercentageEffectSpeed((currentAccelerationTime < 0 ? 0 : currentAccelerationTime) / maxAccelerationTime * 100);
         snowRide.SetIsOnIce(isOnIce);
+    }
+
+    private void SetTrainState()
+    {
+        switch (trainMovement)
+        {
+            case TrainMovement.Accelerating:
+                {
+                    if (InputManager.Data.moveY > 0)
+                    {
+                        currentAccelerationTime = currentAccelerationTime >= maxAccelerationTime ?
+                            maxAccelerationTime :
+                            currentAccelerationTime + (Time.fixedDeltaTime * iceDecelerationMultiplier);
+                    }
+                    else if (currentAccelerationTime >= 0)
+                    {
+                        trainMovement = TrainMovement.Decelerating;
+                    }
+                    break;
+                }
+            case TrainMovement.Decelerating:
+                {
+                    if (InputManager.Data.moveY > 0)
+                    {
+                        trainMovement = TrainMovement.Accelerating;
+                    }
+                    if (currentAccelerationTime > 0)
+                    {
+                        currentAccelerationTime = currentAccelerationTime <= 0 ? 0 : currentAccelerationTime - (Time.fixedDeltaTime / iceDecelerationMultiplier);
+                        if (rb.velocity.magnitude < wallHitVelocityThreshold)
+                            currentAccelerationTime = 0;
+                    }
+                    else
+                        trainMovement = TrainMovement.Stopped;
+                    break;
+                }
+            case TrainMovement.Stopped:
+                {
+                    if (InputManager.Data.moveY > 0)
+                    {
+                        trainMovement = TrainMovement.Accelerating;
+                    }
+                    else if (InputManager.Data.moveY < 0)
+                    {
+                        trainMovement = TrainMovement.Reversing;
+                    }
+                    break;
+                }
+            case TrainMovement.Reversing:
+                {
+                    if (InputManager.Data.moveY < 0)
+                    {
+                        reverseAccelerationTime = reverseAccelerationTime >= maxReverseAccelerationTime ?
+                            maxReverseAccelerationTime :
+                            reverseAccelerationTime + Time.fixedDeltaTime;
+                    }
+                    else if (reverseAccelerationTime > 0)
+                    {
+                        reverseAccelerationTime = reverseAccelerationTime <= 0 ?
+                            0 :
+                            reverseAccelerationTime - Time.fixedDeltaTime * 2;
+                    }
+                    else
+                        trainMovement = TrainMovement.Stopped;
+                    break;
+                }
+        }
     }
 
     private void Turning()
@@ -142,5 +225,6 @@ public enum TrainMovement
 {
     Stopped,
     Accelerating,
-    Decelerating
+    Decelerating,
+    Reversing
 }
