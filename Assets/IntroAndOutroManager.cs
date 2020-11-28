a7965fb03a7e1eeaf15480066d1e75e7fcc379df
Quivscor <quivscor@gmail.com>
2020-11-28 22:37:13 +0100

    Intro and outro work

---
+++ Assets/IntroAndOutroManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroAndOutroManager : MonoBehaviour
+{
+    public List<Cinemachine.CinemachineVirtualCamera> vcams;
+
+    private void Awake()
+    {
+        FindObjectOfType<TracklessGenerator.MapGenerator>().action += SetVirtualCameras;
+    }
+
+    private void SetVirtualCameras()
+    {
+
+    }
+
+    public static void PlayIntroSequence()
+    {
+
+    }
+
+    public static void PlayOutroSequence()
+    {
+
+    }
+}
