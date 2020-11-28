7b9ba93eea8963e3bf4c5091dfa96091d9e8b4c6
Zawisz <zawisza_mateusz@wp.pl>
2020-11-28 02:02:21 +0100

    Wagon spawn improved

--- Assets/Trains/Scripts/Train/TrainManager.cs
+++ Assets/Trains/Scripts/Train/TrainManager.cs
@@ -30,11 +30,9 @@ public class TrainManager : MonoBehaviour
     {
         Wagon lastWagon = wagons[wagons.Count - 1];
 
-        GameObject newWagon = Instantiate(wagon);
-        newWagon.transform.position = lastWagon.jointConnectorEnd.transform.position;
-        newWagon.transform.position -= newWagon.GetComponent<Wagon>().jointConnectorStart.transform.localPosition;
+        GameObject newWagon = Instantiate(wagon, lastWagon.transform.position - new Vector3(0, -50.0f, 0), lastWagon.transform.rotation);
         newWagon.transform.rotation = lastWagon.transform.rotation;
-
+        newWagon.transform.position = lastWagon.jointConnectorEnd.transform.position - (newWagon.GetComponent<Wagon>().jointConnectorStart.transform.position - newWagon.transform.position);
         newWagon.GetComponent<HingeJoint>().connectedBody = lastWagon.jointConnectorEnd.GetComponent<Rigidbody>();
 
         wagons.Add(newWagon.GetComponent<Wagon>());
