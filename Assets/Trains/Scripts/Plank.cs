0a6276b3682af9a4ecf42f80ddd4cb6a30bb2502
Zawisz <zawisza_mateusz@wp.pl>
2020-11-29 01:01:31 +0100

    Destroyanle planks on end column

---
+++ Assets/Trains/Scripts/Plank.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plank : MonoBehaviour
+{
+    public GameObject destroyedPlank;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Locomotive") && other.gameObject.transform.GetComponent<TrainManager>())
+        {
+            GameObject destroyedPlankGO = Instantiate(destroyedPlank, this.transform.position, this.transform.rotation);
+            Destroy(destroyedPlankGO, 4.0f);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Trains/Scripts/PlankDestroyed.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlankDestroyed : MonoBehaviour
+{
+    private void Start()
+    {
+        do
+        {
+            this.transform.GetChild(0).transform.parent = null;
+        } while (this.transform.childCount > 0);
+    }
+}
