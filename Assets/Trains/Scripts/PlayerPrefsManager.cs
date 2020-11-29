95021466be81ccbf841f389fd82fa5d13f6cbc1e
Zawisz <zawisza_mateusz@wp.pl>
2020-11-29 04:38:30 +0100

    PlayerPrefs reload on new game

---
+++ Assets/Trains/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsManager : MonoBehaviour
+{
+    public void ResetPlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
