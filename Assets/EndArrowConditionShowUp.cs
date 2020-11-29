9c6b23a679fd3263cec3418c7a456e1bf9cb455e
Quivscor <quivscor@gmail.com>
2020-11-29 04:51:39 +0100

    EndArrowCondition added

---
+++ Assets/EndArrowConditionShowUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndArrowConditionShowUp : MonoBehaviour
+{
+    private GameObject arrow;
+
+    private void Awake()
+    {
+        arrow = transform.GetChild(0).gameObject;
+    }
+
+    public void ActivateArrow()
+    {
+        arrow.SetActive(true);
+    }
+}
--- Assets/Trains/Scripts/UIDataManager.cs
+++ Assets/Trains/Scripts/UIDataManager.cs
@@ -64,7 +64,10 @@ public class UIDataManager : MonoBehaviour
         steelText.text = inventory.Steel.ToString();
         passengersText.text = inventory.Passengers.ToString();
         if (inventory.Passengers == mapGenerator.numberOfPassengers)
+        {
             TurnOnEndGameText(true, "Great! Find a way out of this place");
+            FindObjectOfType<EndArrowConditionShowUp>().ActivateArrow();
+        }
         //maxPassengersText.text = " / " + mapGenerator.numberOfPassengers;
     }
 
