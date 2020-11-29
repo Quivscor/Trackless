4dd39b8e33b9a1668daa63a44f79fa798e07b646
Zawisz <zawisza_mateusz@wp.pl>
2020-11-29 01:17:04 +0100

    Dynamic train sound

---
+++ Assets/Trains/Scripts/TrainSoundController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainSoundController : MonoBehaviour
+{
+    [SerializeField]
+    private float minPitch = 0.7f;
+
+    [SerializeField]
+    private float maxPitch = 1.2f;
+
+    public AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void ChangeSoundPitchBasedOnVelocity(float percentageSpeed)
+    {
+        if (audioSource)
+        {
+            audioSource.pitch = (maxPitch - minPitch) * percentageSpeed / 100 + minPitch;
+        }
+    }
+}
